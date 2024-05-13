20fb796503ecc8796a993080b36eba59d28a21dc
Emanuel <emednedezdelgado22>
2024-05-13 17:40:24 -0400

    added fiel 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-import sys
-
 if __name__ == "__main__":
-    total_sum = 0
+    import sys
+    sum = 0
     count = len(sys.argv) - 1
     for i in range(count):
-        total_sum += int(sys.argv[i + 1])
-    print("{}".format(total_sum))
+        sum += int(sys.argv[i + 1])
+    print("{}".format(sum))
