edb7ac39ea2a922784068e80faf475b1472ef2db
Emanuel <emednedezdelgado22>
2024-05-13 17:29:31 -0400

    fixed

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+import sys
+
 if __name__ == "__main__":
-    import sys
-    sum = 0
-    for i in range(1, len(sys.argv)):
-        sum += int(sys.argv[i])
-    print(sum)
+    total_sum = 0
+    count = len(sys.argv) - 1
+    for i in range(count):
+        total_sum += int(sys.argv[i + 1])
+    print("{}".format(total_sum))
