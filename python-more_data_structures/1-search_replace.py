ec43a4f95c849a7b087eb83532639346f6f7b04a
Emanuel <emednedezdelgado22>
2024-05-14 16:56:45 -0400

    fixed 1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replace):
+def search_replace(my_list, search_term, replace_term):
     index = 0
     new_list = my_list.copy()
-    for i in my_list:
-        if i == search:
-            new_list[index] = replace
+    for item in my_list:
+        if item == search_term:
+            new_list[index] = replace_term
         index += 1
     return new_list
