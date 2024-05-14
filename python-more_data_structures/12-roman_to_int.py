36df2fe091c0149cf01df259fa5f384d47ee009f
Emanuel <emednedezdelgado22>
2024-05-14 16:45:24 -0400

    added file 10-12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not(isinstance(roman_string, string) or roman+string == None):
+        return 0
+    dict = {"I": 1, "V": 5, "X": 10, "L": 50,\
+                "C": 100, "D": 500, "M": 1000}
