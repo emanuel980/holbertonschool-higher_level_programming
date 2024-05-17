3ad23aa688ef665fca6e22e7687448bf583291e0
Emanuel <emednedezdelgado22>
2024-05-17 17:18:46 -0400

    fixed file 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-"""This module defines a square object"""
+"""" 6. Coordinates of a square
+A class Square that defines a square by: (based on 5-square.py)
+"""
 
 
 class Square:
-
-    """class Square contains method defining attribute size"""
+    """Class Square that defines a square"""
     def __init__(self, size=0, position=(0, 0)):
-        """initialize method for storing attribute"""
-        self.__size = size
-        self.__position = position
+        """Define Private instance attribute: size"""
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
+        """Private instance attribute: size"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
+        """Private instance attribute: size"""
+        if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
@@ -24,24 +27,54 @@ class Square:
 
     @property
     def position(self):
+        """Private instance attribute: position"""
         return self.__position
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple) or\
-                len(value) != 2 or\
-                not isinstance(value[0], int) or\
-                not isinstance(value[1], int) or\
-                value[0] < 0 or value[1] < 0:
+        """Private instance attribute: position"""
+        if type(value) is not tuple or len(value) != 2 or \
+           type(value[0]) is not int or type(value[1]) is not int or \
+           value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
-        return self.__size ** 2
+        """Returns the current square area"""
+        return self.__size * self.__size
 
     def my_print(self):
-        """prints a square"""
-        for i in range(self.__size):
-            for j in range(self.__size):
-                print("#", end="")
-            print("")
+        """Prints in stdout the square with the character #"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                print(" " * self.__position[0], end="")
+                print("#" * self.__size)
+
+    def pos_print(self):
+        """"Function that returns the position of the square"""
+
+        s_position = ""
+        if self.size == 0:
+            return "\n"
+
+        for n in range(self.position[1]):
+            s_position += "\n"
+
+        for n in range(self.size):
+            for x in range(self.position[0]):
+                s_position += " "
+
+            for y in range(self.size):
+                s_position += "#"
+
+            s_position += "\n"
+
+        return s_position
+
+    def __str__(self):
+        """Prints in stdout the square with the character #"""
+        return self.pos_print()
