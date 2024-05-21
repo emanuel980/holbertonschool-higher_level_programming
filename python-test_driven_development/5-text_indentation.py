4c47a1d5d6bb26891035be1122fd0a143c993185
Emanuel <emednedezdelgado22>
2024-05-21 15:19:41 -0400

    deleted file

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""Text indentation module"""
+"""text indent"""
 
 
 def text_indentation(text):
-    """Prints a text with 2 new lines after each characters: ., ? and :
-    Args:
-        text (str): The text to print.
-    Raises:
-        TypeError: If text is not a string.
-    Returns:
-        None: The text is printed.
-    """
+    """text indent"""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    text = text.replace('.', '.\n\n')
-    text = text.replace('?', '?\n\n')
-    text = text.replace(':', ':\n\n')
-    print("\n".join([line.strip() for line in text.split("\n")]), end="")
+
+    for delimeter in "?:.":
+        words = (delimeter + "\n\n").join(
+                [index.strip(" ") for index in words.split(delimeter)])
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
