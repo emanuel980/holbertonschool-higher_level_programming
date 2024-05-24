88676124c5101d3500f3084e13421ae7ea62c2ed
Emanuel <emednedezdelgado22>
2024-05-24 17:01:49 -0400

    changed file

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,23 +1,35 @@
-#!/usr/bin/python3
-
 class CountedIterator:
-
     def __init__(self, iterable):
         self.iterator = iter(iterable)
-        self.__count = 0
+        self.count = 0
+
+    def __next__(self):
+        try:
+            item = next(self.iterator)
+            self.count += 1
+            return item
+        except StopIteration:
+            raise StopIteration("No more items to iterate")
 
     def get_count(self):
-        return self.__count
-    
-    def __iter__(self):
-        return self
-    
-    def __next__(self):
-        self.__count += 1
-        item = next(self.iterator)
-        return item
-        
-        
+        return self.count
 
-        
-    
+# Testing the CountedIterator
+def main():
+    # Instantiate a CountedIterator object using a list
+    my_list = [1, 2, 3, 4, 5]
+    counted_iter = CountedIterator(my_list)
+
+    # Iterate over items
+    try:
+        while True:
+            item = next(counted_iter)
+            print(item)
+    except StopIteration as e:
+        print(e)
+
+    # Print the number of items fetched
+    print("Count:", counted_iter.get_count())
+
+if __name__ == "__main__":
+    main()
