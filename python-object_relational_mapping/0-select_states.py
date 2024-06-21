a2cc0fc3b32411c0686f8c6f4d6414c6bf77667c
Emanuel <emednedezdelgado22>
2024-06-21 15:58:15 -0400

    trouble shooting

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,30 +1,43 @@
 #!/usr/bin/python3
 """
-Script that lists all states from the databse hbtn_0e_0_usa
+Script that lists all states from the database hbtn_0e_0_usa
 """
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
+    if len(argv) != 4:
+        print("Usage: ./<script_name>.py <username> <password> <database_name>")
+        exit(1)
+    
     u_name = argv[1]
     psw = argv[2]
     base = argv[3]
 
-    # Connecting to MySQL database
-    db = MySQLdb.connect(host="localhost", user=u_name,
-                         passwd=psw, db=base, port=3306)
+    try:
+        # Connecting to MySQL database
+        db = MySQLdb.connect(host="localhost", user=u_name,
+                             passwd=psw, db=base, port=3306)
+    except MySQLdb.Error as err:
+        print(f"Error: {err}")
+        exit(1)
 
-    # Creating cursor object
-    cur = db.cursor()
+    try:
+        # Creating cursor object
+        cur = db.cursor()
 
-    # Executing MySql Query
-    cur.execute("SELECT * FROM states ORDER BY id")
+        # Executing MySQL Query
+        cur.execute("SELECT * FROM states ORDER BY id")
 
-    # Obtaining Query Result & prints the result in rows
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+        # Obtaining Query Result & prints the result in rows
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
 
-    # Clean Up
-    cur.close()
-    db.close()
+    except MySQLdb.Error as err:
+        print(f"Error: {err}")
+
+    finally:
+        # Clean Up
+        cur.close()
+        db.close()
