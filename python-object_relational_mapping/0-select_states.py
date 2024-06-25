8b92235a58783e085f3058ec6de10b3db039d646
Emanuel <emednedezdelgado22>
2024-06-25 18:49:52 -0400

    fixed file

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -16,20 +16,25 @@ if __name__ == '__main__':
 
     try:
         # Connecting to MySQL database
+        print("Connecting to database...")
         db = MySQLdb.connect(host="localhost", user=u_name,
                              passwd=psw, db=base, port=3306)
+        print("Connected to database")
     except MySQLdb.Error as err:
         print(f"Error: {err}")
         exit(1)
 
     try:
         # Creating cursor object
+        print("Creating cursor...")
         cur = db.cursor()
 
         # Executing MySQL Query
+        print("Executing query...")
         cur.execute("SELECT * FROM states ORDER BY id")
 
         # Obtaining Query Result & prints the result in rows
+        print("Fetching rows...")
         rows = cur.fetchall()
         for row in rows:
             print(row)
@@ -39,5 +44,6 @@ if __name__ == '__main__':
 
     finally:
         # Clean Up
+        print("Closing cursor and database connection...")
         cur.close()
         db.close()
